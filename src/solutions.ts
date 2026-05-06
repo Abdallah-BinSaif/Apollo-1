825adf65cd3593e7dc9d90ebdc2e3661e0c56d38
Abdallah-BinSaif <binsaif.info@gmail.com>
2026-05-06 23:50:09 +0600

    Solution of First Problem

---
+++ src/solutions.ts
@@ -0,0 +1,15 @@
+function terminal (value: any){
+    console.log(value);
+}
+
+
+// Problem - 1
+
+function filterEvenNumbers(array:number[]):number[]{
+    return [] = array.filter((a)=> a % 2 === 0)
+}
+
+const result = filterEvenNumbers([1, 2, 3, 4, 5, 6])
+
+
+// Problem - 2
